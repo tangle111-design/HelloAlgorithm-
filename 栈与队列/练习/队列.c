69f4ecc0450129c594d4b0601ca8262a813675c5
tangle_T^T <tangle_111@163.com>
2026-04-21 13:19:50 +0800

    完成队列

--- 栈与队列/练习/队列.c
+++ 栈与队列/练习/队列.c
@@ -1,36 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
 /* 练习版：基于链表实现的队列 */
 #include <stdbool.h>
 
-typedef struct ListNode ListNode;
+typedef struct ListNode
+{
+    int val;
+    struct ListNode *next;
+} ListNode;
 
-typedef struct {
+typedef struct
+{
     ListNode *front, *rear;
     int queSize;
 } LinkedListQueue;
 
 /* 构造函数 */
-LinkedListQueue *newLinkedListQueue();
+LinkedListQueue *newLinkedListQueue()
+{
+    LinkedListQueue *newone = malloc(sizeof(LinkedListQueue));
+    newone->queSize = 0;
+    newone->front = NULL;
+    newone->rear = NULL;
+    return newone;
+}
 
 /* 析构函数 */
-void delLinkedListQueue(LinkedListQueue *queue);
+void delLinkedListQueue(LinkedListQueue *queue)
+{
+    while (queue->front != NULL)
+    {
+        ListNode *temp = queue->front;
+        queue->front = queue->front->next;
+        free(temp);
+    }
+    free(queue);
+}
 
 /* 获取队列的长度 */
-int size(LinkedListQueue *queue);
+int size(LinkedListQueue *queue)
+{
+    return queue->queSize;
+}
 
 /* 判断队列是否为空 */
-bool empty(LinkedListQueue *queue);
+bool empty(LinkedListQueue *queue)
+{
+    return queue->queSize == 0;
+}
 
 /* 入队 */
-void push(LinkedListQueue *queue, int num);
+void push(LinkedListQueue *queue, int num)
+{
+    ListNode *newnode = malloc(sizeof(ListNode));
+    newnode->val = num;
+    newnode->next = NULL;
+    if (!empty(queue))
+    {
+        queue->rear->next = newnode;
+        queue->rear = queue->rear->next;
+    }
+    else
+    {
+        // 空队列时，新节点应同时成为队首和队尾，即 front 和 rear 都要指向它。
+        queue->rear = newnode;
+        queue->front = newnode;
+    }
+    queue->queSize++;
+}
 
 /* 访问队首元素 */
-int peek(LinkedListQueue *queue);
+int peek(LinkedListQueue *queue)
+{
+    if (empty(queue))
+    {
+        printf("empty");
+        return -1;
+    }
+    return queue->front->val;
+}
 
 /* 出队 */
-int pop(LinkedListQueue *queue);
+int pop(LinkedListQueue *queue)
+{
+    if (empty(queue))
+    {
+        printf("empty");
+        return -1;
+    }
+    int res = queue->front->val;
+    ListNode *temp = queue->front;
+    queue->front = queue->front->next;
+    free(temp);
+    queue->queSize--;
+    if (queue->queSize == 0)
+    {
+        queue->rear = NULL;
+    }
+    return res;
+}
 
 /* 打印队列 */
-void printLinkedListQueue(LinkedListQueue *queue);
+void printLinkedListQueue(LinkedListQueue *queue)
+{
+    ListNode *node = queue->front;
+    while (node != NULL)
+    {
+        printf("%d ", node->val);
+        node = node->next;
+    }
+}
 
 /* 练习提示：
  * 1. 先区分空队列和非空队列。
--- 栈与队列/练习/队列.py
+++ 栈与队列/练习/队列.py
@@ -6,7 +6,7 @@ class ArrayQueue:
     2. 队尾索引可以通过 (front + size) % capacity 计算。
     3. 注意空队列和满队列的边界处理。
     """
-    
+
     def __init__(self, size: int):
         """初始化练习结构。
 
@@ -14,15 +14,22 @@ class ArrayQueue:
         1. 分配固定长度数组。
         2. 将队首指针和长度初始化为 0。
         """
+        self._nums: list[int] = [0] * size
+        self._front: int = 0
+        self._size: int = 0
+
 
     def capacity(self) -> int:
         """获取队列的容量。"""
+        return len(self._nums)
 
     def size(self) -> int:
         """获取队列的长度。"""
+        return self._size
 
     def is_empty(self) -> bool:
         """判断队列是否为空。"""
+        return self._size == 0
 
     def push(self, num: int):
         """入队。
@@ -32,6 +39,12 @@ class ArrayQueue:
         2. 根据 front 和 size 计算队尾位置。
         3. 写入元素后再更新 size。
         """
+        if(self._size == len(self._nums)):
+            raise IndexError("full")
+        # 尾巴的计算, size刚好多1, 所以下标不用再+1. 将数组视作环形的, 节省空间
+        rear: int = (self._front + self._size) % self.capacity()
+        self._nums[rear] = num
+        self._size += 1
 
     def pop(self) -> int:
         """出队。
@@ -41,6 +54,12 @@ class ArrayQueue:
         2. 再移动队首指针。
         3. 最后减少 size。
         """
+        res: int = self.peek()
+        self._front = (self._front + 1) % self.capacity()
+        self._size -= 1
+        return res
+
+        
 
     def peek(self) -> int:
         """访问队首元素。
@@ -49,6 +68,11 @@ class ArrayQueue:
         1. 先判断队列是否为空。
         2. 返回 front 位置的元素。
         """
+        if self.is_empty():
+            raise IndexError("empty")
+        return self._nums[self._front]
+        
+        
 
     def to_list(self) -> list[int]:
         """转换为列表，方便打印和调试。
@@ -57,3 +81,7 @@ class ArrayQueue:
         1. 从 front 开始遍历 size 个元素。
         2. 使用取余处理环形数组回绕。
         """
+        res: list[int] = [] 
+        for i in range(self.size()):
+            res.append(self._nums[(self._front + i) % self.capacity()])
+        return res
